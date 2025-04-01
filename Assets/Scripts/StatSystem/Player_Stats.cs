30be50dabacc5fd45fd99152ba9c922dfff7c479
s27502 <161813707+s27502@users.noreply.github.com>
2025-04-01 17:07:13 +0200

    Moved Stats to it's own script file

--- Assets/Scripts/StatSystem/Player_Stats.cs
+++ Assets/Scripts/StatSystem/Player_Stats.cs
@@ -2,21 +2,22 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Player_Stats : MonoBehaviour
 {
-    public Stat MaxHealth = new Stat { BaseValue = 100 };
-    public Stat Health = new Stat { BaseValue = 100 };
-    public Stat Strength = new Stat { BaseValue = 10 };
-    public Stat Agility = new Stat { BaseValue = 10 };
-    public Stat Intelligence = new Stat { BaseValue = 10 };
-    public Stat Coins = new Stat { BaseValue = 0 };
-    public Stat Bombs = new Stat { BaseValue = 0 };
-    public Stat Keys = new Stat { BaseValue = 0 };
+    public Stat MaxHealth = new Stat(110, StatType.MaxHealth);
+    public Stat Health = new Stat(100, StatType.Health);
+    public Stat Strength = new Stat(10, StatType.Strength);
+    public Stat Agility = new Stat(10, StatType.Agility);
+    public Stat Intelligence = new Stat(10, StatType.Intelligence);
+    public Stat Coins = new Stat (0, StatType.Coin);
+    public Stat Bombs = new Stat (0, StatType.Bomb);
+    public Stat Keys = new Stat (0, StatType.Key);
     [SerializeField] private InventoryUI inventoryUI;
-    
-    
-    
+
+   
+
     void Update()
     {
         if (Input.GetButtonDown("Jump"))
@@ -27,24 +28,15 @@ public class Player_Stats : MonoBehaviour
     }
 }
 
-[System.Serializable]
-public class Stat
+public enum StatType
 {
-    public int BaseValue;
-    private int _modifiedValue;
-
-    public int Value
-    {
-        get { return BaseValue + _modifiedValue; }
-    }
-
-    public void Modify(int amount)
-    {
-        _modifiedValue += amount;
-    }
-
-    public void ResetModifiers()
-    {
-        _modifiedValue = 0;
-    }
+    Health,
+    MaxHealth,
+    Strength,
+    Agility,
+    Intelligence,
+    Coin,
+    Bomb,
+    Key
+    
 }
