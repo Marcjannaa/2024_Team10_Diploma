060275f56dddd22b514c453d8395fb5c0dcf43d7
Marcjannaa <32717296+Marcjannaa@users.noreply.github.com>
2025-04-07 23:16:02 +0200

    player placeholder

--- Assets/Scripts/Player/ColorMode.cs
+++ Assets/Scripts/Player/ColorMode.cs
@@ -1,88 +1,80 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor;
 using UnityEngine;
-using UnityEngine.Experimental.GlobalIllumination;
-using UnityEngine.Rendering.Universal;
 
 public class ColorMode : MonoBehaviour
 {
-    public enum PlayerState { Attacking, Idle, HighHp, MediumHp, LowHp } // w zaleznosci od zabitych przeciwnikow, zebranych deadrosy
-    
-    [SerializeField] private Light2D ld;
-    [SerializeField] private float intensity = 0.5f;
-    private PlayerState _playerState;
-    private float _alpha;
+    public enum PlayerState { Attacking, Idle, HighHp, MediumHp, LowHp }
+
+    [SerializeField] private Light ld;
+    [SerializeField] private float intensity = 5f;
+
+    private float _alpha = 1f;
     private ParticleSystem _particleSystem;
     private ParticleSystem.MainModule _main;
+
     private void Start()
     {
-        _particleSystem = gameObject.GetComponent<ParticleSystem>();
+        _particleSystem = GetComponent<ParticleSystem>();
+        if (_particleSystem == null)
+        {
+            Debug.LogError("Brak komponentu ParticleSystem!");
+            return;
+        }
+
         _main = _particleSystem.main;
-        ld.color = PickColor(_playerState);
+
         SetParticles();
-        ld.intensity = intensity;
-        ld.falloffIntensity = 1f;
-        ld.volumeIntensityEnabled = true;
-        ld.volumeIntensity = 0.0001f;
+        SetParticleColor(PickColor(PlayerState.HighHp));
     }
 
     private void SetParticles()
     {
-        _main.startColor = PickColor(_playerState);
         _main.loop = true;
-        _main.simulationSpace = ParticleSystemSimulationSpace.World;
+        _main.simulationSpace = ParticleSystemSimulationSpace.Local; 
         _main.startLifetime = 2f;
         _main.playOnAwake = true;
+
         _main.startSize = 1f;
         _main.startSpeed = 0.5f;
-        _main.maxParticles = 100;
+        _main.maxParticles = 200;
+
+        var emission = _particleSystem.emission;
+        emission.rateOverTime = 50f;
+
+        var shape = _particleSystem.shape;
+        shape.enabled = true;
+        shape.shapeType = ParticleSystemShapeType.Circle;
     }
-    public void UpdateColor()
-    {
-        /*
-        ld.color = PickColor(_playerState);
-        SetParticleColor(PickColor(_playerState));
-        var hp = gameObject.GetComponentInParent<PlayerStats>().getCurrentHp();
-        _alpha = 255f * hp;
-        intensity *= hp;
-        var hearts = gameObject.GetComponentInParent<PlayerStats>().getHearts();
-        _playerState = hearts switch 
-        {
-                1 => PlayerState.LowHp,
-                2 => PlayerState.MediumHp,
-                3 => PlayerState.HighHp,
-                _ => _playerState 
-        };
-        ld.color = PickColor(_playerState);
-        SetParticleColor(PickColor(_playerState));
-    */
-    }
+
+
 
     private Color PickColor(PlayerState ps)
     {
         return ps switch
         {
-            PlayerState.HighHp => new Color(60f, 0f, 255f, _alpha),
-            PlayerState.MediumHp => new Color(255f, 100f, 0f, _alpha),
-            PlayerState.LowHp => new Color(200f, 0f, 0f, _alpha),
-            _ => new Color(255f, 255f, 255f, _alpha)
+            PlayerState.HighHp => new Color(200f / 255f, 0f, 255f / 255f, _alpha),
+            PlayerState.MediumHp => new Color(255f / 255f, 100f / 255f, 0f, _alpha),
+            PlayerState.LowHp => new Color(200f / 255f, 0f, 0f, _alpha),
+            _ => new Color(1f, 1f, 1f, _alpha)
         };
     }
-    
+
     private void SetParticleColor(Color color)
     {
-        var mainModule = GetComponent<ParticleSystem>().main;
         var gradient = new Gradient();
         GradientColorKey[] colorKeys = new GradientColorKey[2];
-        colorKeys[0] = new GradientColorKey(color, 0.0f); 
-        colorKeys[1] = new GradientColorKey(new Color(color.r / 10, color.g / 10, color.b / 10, _alpha), 0.5f);  
-        
-        gradient.colorKeys = colorKeys;
-        mainModule.startColor = new ParticleSystem.MinMaxGradient(gradient);
-    }
+        GradientAlphaKey[] alphaKeys = new GradientAlphaKey[2];
 
-    public void SetPlayerState(PlayerState ps) { _playerState = ps; }
+        colorKeys[0] = new GradientColorKey(color, 0.0f);
+        colorKeys[1] = new GradientColorKey(new Color(color.r / 10f, color.g / 10f, color.b / 10f), 1.0f);
+
+        alphaKeys[0] = new GradientAlphaKey(color.a, 0.0f);
+        alphaKeys[1] = new GradientAlphaKey(color.a, 1.0f);
+
+        gradient.colorKeys = colorKeys;
+        gradient.alphaKeys = alphaKeys;
+
+        _main.startColor = new ParticleSystem.MinMaxGradient(gradient);
+    }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,29 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     [SerializeField] float _speed = 5.0f;
     private bool _canCombat = false;
     private GameObject _enemyInRange = null;
-    
+    [SerializeField] private Animator anim;
     void Update ()
         {
             float horizontal = Input.GetAxis("Horizontal");
             float vertical = Input.GetAxis("Vertical");
 
+
+            if (horizontal != 0 || vertical != 0)
+            {
+                anim.SetBool("IsRunning", true);
+            }
+            else
+            {
+                anim.SetBool("IsRunning", false);
+            }
+            gameObject.GetComponentInChildren<PlayerSprites>().LookLeft(Input.GetAxis("Horizontal") < 0);
+
+            
             var direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             
             transform.Translate(direction * _speed * Time.deltaTime);
