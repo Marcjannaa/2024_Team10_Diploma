ff8f520e8b178030c826a252570fac89961531cb
s26697 <s26697@pjwstk.edu.pl>
2025-04-15 22:07:31 +0200

    removed unused variables from: roomConfig, ExitPoint, PlacedRoom

--- Assets/Scripts/ProceduralGeneration/RoomConfig.cs
+++ Assets/Scripts/ProceduralGeneration/RoomConfig.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Dungeon/Room Config")]
 public class RoomConfig : ScriptableObject
 {
-    public string roomName;
     public GameObject prefab;
-    public bool isSpecialRoom;
-    public bool isBossRoom;
+    
 }
