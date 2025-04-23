09768a72a1d8d4b25e9146c9399bb626e3093341
s26697 <s26697@pjwstk.edu.pl>
2025-04-23 04:34:28 +0200

    fixed combat manager imports to let it build

--- Assets/Scripts/CombatManager.cs
+++ Assets/Scripts/CombatManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using TMPro;
 using Unity.VisualScripting;
-using UnityEditor.Build;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
