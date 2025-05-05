ad113478f33b7cb54ade61e4fdc40ac8316e47ef
s27466 <s27466@pjwstk.edu.pl>
2025-05-05 23:40:24 +0200

    spikes prefab

--- Assets/GroundButton.cs
+++ Assets/GroundButton.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 
 public class GroundButton : MonoBehaviour
 {
+    private GameObject _spikeSet;
     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            print("OnTriggerEnter");
+            transform.Find("Unpressed").gameObject.SetActive(false);
+            transform.Find("Pressed").gameObject.SetActive(true);
             
+            _spikeSet.SetActive(false);
         }
     }
 }
