84348403864bfdc30ea67f14b61eaaebc2413e39
s26697 <s26697@pjwstk.edu.pl>
2025-10-05 22:09:48 +0200

    Add ArcDoors prefab and room completion logic
    Introduced ArcDoors prefab with animator and scripts for procedural generation. Added EnemyOnDefeatScript to trigger events on enemy defeat, OpenCloseRoomExitScript to control door animations, and RoomCompletionScript to manage room completion based on enemy defeat. Updated StandardSkinnyRoom 1 prefab to use new ArcDoors and scripts, replacing old arcway references and wiring up enemy defeat events to room completion logic. Updated ArcDoors animator controller and FBX meta to support open/close triggers and animations.

---
+++ Assets/Scripts/ProceduralGeneration/OpenCloseRoomExitScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace ProceduralGeneration
+{
+    public class OpenCloseRoomExitScript : MonoBehaviour
+    {
+        private Animator anim;
+        
+        private void Awake()
+        {
+            anim = GetComponent<Animator>();
+        }
+        
+        public void OpenRoomExit()
+        {
+            anim.SetTrigger("ArcDoorOpenTrigger");
+            Debug.Log("room in now open");
+        }
+        
+        public void CloseRoomExit()
+        {
+            anim.SetTrigger("ArcDoorCloseTrigger");
+            Debug.Log("room in now closed");
+        }
+    }
+}
---
+++ Assets/Scripts/ProceduralGeneration/RoomCompletionScript.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace ProceduralGeneration
+{
+    public class RoomCompletionScript : MonoBehaviour
+    {
+        private bool isRoomComplete = false;
+        
+        [SerializeField] private GameObject[] enemyList;
+        [SerializeField] private GameObject[] exitList;
+        private int enemyCount;
+        private void Awake()
+        {
+            enemyCount = enemyList.Length;
+        }
+        
+       
+        private void OnTriggerEnter(Collider other)
+        {
+            if (isRoomComplete)
+            {
+                return;
+            }
+
+            if (enemyCount <= 0)
+            {
+                return;
+            }
+            foreach (var door in exitList)
+            {
+                door.GetComponent<OpenCloseRoomExitScript>().CloseRoomExit();
+            }
+        }
+
+        public void EnemyKilled()
+        {
+            if (enemyCount > 1)
+            {
+                enemyCount--;
+            }
+            else
+            {
+                isRoomComplete = true;
+
+                foreach (var door in exitList)
+                {
+                    door.GetComponent<OpenCloseRoomExitScript>().OpenRoomExit();
+                }
+            }
+            
+        }
+    }
+}
