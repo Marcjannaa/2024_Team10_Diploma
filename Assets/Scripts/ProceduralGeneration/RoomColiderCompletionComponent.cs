133d3ce253742995e6c54578b2ada5dfa17b21bc
s26697 <s26697@pjwstk.edu.pl>
2025-10-11 17:27:09 +0200

    Update RoomColiderCompletionComponent.cs

---
+++ Assets/Scripts/ProceduralGeneration/RoomColiderCompletionComponent.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace ProceduralGeneration
+{
+    public class RoomColiderCompletionComponent : MonoBehaviour
+    {
+        private bool isRoomComplete = false;
+
+        [SerializeField] private GameObject[] enemyList;
+        [SerializeField] private GameObject[] exitList;
+        private int enemyCount;
+
+        private void Awake()
+        {
+            enemyCount = enemyList.Length;
+        }
+
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                if (isRoomComplete)
+                {
+                    return;
+                }
+
+                if (enemyCount <= 0)
+                {
+                    return;
+                }
+
+                foreach (var door in exitList)
+                {
+                    door.GetComponent<DoorHandlingComponent>().CloseRoomExit();
+                }
+            }
+        }
+        // Called by UnityEvent in EnemyOnDefeatComponent
+        public void EnemyKilled()
+        {
+            if (enemyCount > 1)
+            {
+                enemyCount--;
+            }
+            else
+            {
+                isRoomComplete = true;
+
+                foreach (var door in exitList)
+                {
+                    door.GetComponent<DoorHandlingComponent>().OpenRoomExit();
+                }
+            }
+        }
+    }
+}
