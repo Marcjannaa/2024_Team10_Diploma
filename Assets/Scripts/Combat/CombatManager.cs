e953f3772bf86797187f30eb4c99ee8987aef7ea
s27466 <161813484+s27466@users.noreply.github.com>
2025-10-13 11:12:06 +0200

    jakieś poprawki

--- Assets/Scripts/Combat/CombatManager.cs
+++ Assets/Scripts/Combat/CombatManager.cs
@@ -191,14 +191,14 @@ public class CombatManager : MonoBehaviour
         Instance._turn = enemyAdvantage ? Turn.Enemy : Turn.Player;
         Instance._enemyFirstStrike = enemyAdvantage;
 
-        Instance.StartCoroutine(Transition.Instance.PlayTransition(() =>
-        {
+        //Instance.StartCoroutine(Transition.Instance.PlayTransition(() =>
+        //{
             _battleUI.SetActive(true);
             Transform battleSpriteTransform = EnemyGO.transform.Find("BattleSprite");
             Instance._enemySprite = battleSpriteTransform.GetComponent<Image>();
             Instance.SwitchBattleUIPanel();
             Instance.StartCoroutine(Instance.BattleLoop());
-        }));
+        //}));
     }
 
     private void EnemyAction()
